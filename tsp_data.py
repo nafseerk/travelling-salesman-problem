95f34f706672821d839594d1d2d7906d3fe6ab27
Apple <apple@v1022-rn-71-94.campus-dynamic.uwaterloo.ca>
2017-09-24 22:01:40 -0400

    added documentation

--- tsp_data.py
+++ tsp_data.py
@@ -1,13 +1,22 @@
 import math
 from tabulate import tabulate
 import itertools
-import pprint
 
 class TSPData:
+    """ The data object that stores all the input data of the TSP Problem -
+        1. The no of cities - citiesCount
+        2. The names/Ids of the cities - citiesNames
+        3. The x,y coordinates of the cities - citiesCoordinates
+    """
     citiesCount = 0
+    citiesNames = []
     citiesCoordinates = {}
     
     def __init__(self, inputFileName):
+        """ Loads the data from the input file. Assumes the format:
+            no of cities (first line),
+            cityId, x, y (on each subsequent line)
+        """ 
         f = open(inputFileName, 'r') 
         lines = f.readlines()
         f.close()
@@ -15,78 +24,85 @@ class TSPData:
         self.citiesCount=int(lines[0])
 
         for line in lines[1:]:
-            cityId, cityX, cityY = line.split(' ') 
+            cityId, cityX, cityY = line.split(' ')
+            self.citiesNames.append(cityId)
             self.citiesCoordinates[cityId] = (int(cityX), int(cityY))
 
         return
 
 
-    def getDistance(self, city1Id, city2Id):
-        #TODO: incorportate float
-        x1, y1 = self.citiesCoordinates[city1Id]
-        x2, y2 = self.citiesCoordinates[city2Id]
+    def getDistance(self, city1, city2):
+        """ Returns the Euclidean distance between city1 and city2"""
+        x1, y1 = self.citiesCoordinates[city1]
+        x2, y2 = self.citiesCoordinates[city2]
         return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
     def getAllCities(self):
-        return list(self.citiesCoordinates.keys())
+        return self.citiesNames
         
     def summary(self):
+        print(15*'=' + "Data Summary" + 15*'=')
         print('No of cities = %d' % self.citiesCount)
-        headers = ['CityId', 'X-coordinate', 'Y-coordinate']
+        headers = ['City', 'X-coordinate', 'Y-coordinate']
         data = sorted([(k,v[0],v[1]) for k,v in self.citiesCoordinates.items()])
         print(tabulate(data, headers=headers))
+        print(42*'=')
 
     def getMinimumSpanningTreeCost(self, citiesList, startCity):
-        pp = pprint.PrettyPrinter(indent=4)
+        """Calculate the cost of all the edges of the Minimum Spanning Tree
+           formed from the graph represented by cities list
+        """
+        #TODO: move this to TSPHelper class
         if len(citiesList) == 0:
             return 0
-
-        #print('Calling MST with startCity ', startCity, ' on')
-        #print(citiesList)
         
         if startCity not in citiesList:
             print('StartCity should be in the cities list')
 
-        #find the distances of all combinations of cities
+        #find the distances of all of city-pairs and sort by the distance
         paths = [(path[0], path[1], self.getDistance(path[0], path[1])) for path in itertools.combinations(citiesList, 2)]
         paths.sort(key=lambda tup: tup[2])
-        #print('printing all paths')
-        #pp.pprint(paths)
-        
+
+        #Progressively build the MST
         mstSoFar = [startCity]
-        mstPathCost = 0
+        mstPathCost = 0 
         del citiesList[citiesList.index(startCity)]
 
         while citiesList:
-            #print('mstSoFar = ')
-            #print(mstSoFar)
-            #from all possible city pairs take only those that connect a city from mstSoFar to a city not in mstSoFar
+            #From all possible city pairs take only those that connect a city from mstSoFar to a city not in mstSoFar
             filteredPaths = []
             for path in paths:
                 if (path[0] in mstSoFar and path[1] not in mstSoFar) or (path[1] in mstSoFar and path[0] not in mstSoFar):
                     filteredPaths.append(path)                   
             filteredPaths = sorted(filteredPaths, key=lambda tup: tup[2])
-            #print('filtered paths = ')
-            #pp.pprint(filteredPaths)
-            nextPath = filteredPaths.pop(0)
-            #print('nextpath = ')
-            #pp.pprint(nextPath)
+            
+            nextPath = filteredPaths.pop(0) #of the paths that connect mstSoFar to the rest of the graph, take the lowest cost edge
+
+            #Get the next city to be added into the MST
             if (nextPath[0] in mstSoFar) and (nextPath[1] not in mstSoFar):
                 nextCity = nextPath[1]
             elif (nextPath[1] in mstSoFar) and (nextPath[0] not in mstSoFar):
                 nextCity = nextPath[0]
             mstPathCost += nextPath[2]
             mstSoFar.append(nextCity)
-            #print('nextCity from mst = ' + nextCity)
             del citiesList[citiesList.index(nextCity)]
             paths = [path for path in paths if path != nextPath]
-            #print('after removing the selected path')
-            #pp.pprint(paths)
         return mstPathCost
                     
 if __name__ == '__main__':
-    cities = TSPData('/Users/apple/Documents/Projects/randTSP/4/instance_3.txt')
-    cities.summary()
-    #print("distance from A to B = %d" % cities.getDistance('A', 'B'))
-    print(cities.getMinimumSpanningTreeCost(cities.getAllCities(), 'A'))
-    #print(cities.getAllCities())
+    #Load the data from the input file
+    testData = TSPData('/Users/apple/Documents/git-repos/tsp/travelling-salesman-problem/input-data/8/instance_5.txt')
+
+    #Print the data summary
+    testData.summary()
+
+    #Print all the city names
+    citiesList = testData.getAllCities()
+    print('List of Cities =', end=' ')
+    print(citiesList)
+
+    #Print the distance between any two cities
+    print('Distance from %s to %s = %d' % (citiesList[0], citiesList[1], testData.getDistance(citiesList[0], citiesList[1])))
+
+    #Print the minimum Spanning Tree Cost of the entire tree
+    print('Minimum Spanning Tree Cost = %f' % testData.getMinimumSpanningTreeCost(citiesList, citiesList[0]))
